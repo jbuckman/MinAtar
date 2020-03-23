f6ec383525e25005683ad43f246291e373e0c461
jbuckman <jacobbuckman@gmail.com>
2020-03-23 13:34:30 -0400

    fixed path

--- minatar/pseudorandom.py
+++ minatar/pseudorandom.py
@@ -1,5 +1,5 @@
-import numpy
-with open("minatar/random.npy", "rb") as f: RANDOM = numpy.load(f)
+import numpy, os
+with open(os.path.join(os.path.dirname(__file__), "random.npy"), "rb") as f: RANDOM = numpy.load(f)
 
 def seeded_randint(seed, min, max):
     return numpy.cast[numpy.int64](RANDOM[seed % RANDOM.shape[0]] % (max - min) + min)
